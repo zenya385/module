a74e799b286d3a486c7d270c34a2754a2c730645
Zeno Roman <zeno385@gmail.com>
2023-06-26 11:29:09 +0300

    first commit

---
+++ js/functions.js
@@ -0,0 +1,10 @@
+export function sum(a, b) {
+  a + b;
+}
+
+export function total(x, y) {
+  x * y;
+}
+
+//
+// export default total;
---
+++ main.js
@@ -0,0 +1,13 @@
+import sum from "./js/functions.js";
+import total from "./js/functions.js";
+
+// sum(23, 5);
+// total(18, 48);
+
+// sum(256, 5);
+// total(186, 98);
+
+console.log("sum(23, 5):>>", sum(23, 5));
+console.log("total(18, 48):>>", total(18, 48));
+console.log("sum(256, 5):>>", sum(256, 5));
+console.log("total(186, 98):>>", total(186, 98));
